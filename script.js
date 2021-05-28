d377e9e72d5b817fed64c22896034cc8fa7eb0e5
Frodevargen <62752310+Frodevargen@users.noreply.github.com>
2021-05-28 23:24:59 +0200

    Keylime Pi

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-let mountain = document.getElementById("mountain"); let road = document.getElementById("road");
+let build3= document.getElementById("build3"); let build1 = document.getElementById("build1");
+let parallax1 = document.getElementById("parallax1");
+let section = document.getElementById("section:after");
 window.addEventListener('scroll', function(){
   var value = window.scrollY;
-  mountain.style.top = -value * 0.25 + 'px'; road.style.top = value * 1.15 + 'px';
+  build3.style.top = -value * 0.25 + 'px'; build1.style.top = value * 1.15 + 'px';
     })
+var currentcolor = localStorage.getItem('color');
+if(currentcolor=="red"){
+  document.documentElement.style.setProperty("--color","red");
+  console.log("FUck Im RED")
+  document.documentElement.style.setProperty("--color2","darkturquoise");
+
+}
+let btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', function() {
+  switch (currentcolor) {
+    case "darkturquoise":
+    document.documentElement.style.setProperty("--color","red");
+    console.log("FUck Im RED")
+    document.documentElement.style.setProperty("--color2","darkturquoise");
+    currentcolor = "red";
+    localStorage.setItem('color',currentcolor);
+      break;
+
+    case "red":
+      document.documentElement.style.setProperty("--color","darkturquoise");
+      console.log("FUck im Blue")
+      document.documentElement.style.setProperty("--color2","red");
+      currentcolor = "darkturquoise";
+      localStorage.setItem('color',currentcolor);
+        break;
+    default:
+    localStorage.setItem('color','darkturquoise');
+
+  }
+
+});
+//This is all thanks to Keylime Pi#5573 a real fuqing gamer
